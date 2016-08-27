a41d5726f85431fecaee497722ca883435243671
gbuomprisco <giancarlopsk@gmail.com>
2016-08-27 13:12:37 +0100

    - Fixed critical bug

--- src/components/menu/ng2-dropdown-menu.ts
+++ src/components/menu/ng2-dropdown-menu.ts
@@ -156,6 +156,6 @@ export class Ng2DropdownMenu {
         const body = document.querySelector('body');
         body.appendChild(this.element.nativeElement);
 
-        this.renderer.listen(body, 'keyup', this.handleKeypress);
+        this.renderer.listen(body, 'keyup', this.handleKeypress.bind(this));
     }
 }
