aebfdc48f52b06ababcad36abf3afc7d9ae149ea
Gbuomprisco <giancarlopsk@gmail.com>
2016-09-03 02:27:04 +0100

    Revert "Compilation issues"
    This reverts commit efd2bf0bc0aead3371e4aa6a1c215dac8cf00ba5.
    
    Conflicts:
    	package.json

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare const Ng2Dropdown: any;
+declare const Ng2DropdownMenu: any;
+declare const Ng2MenuItem: any;
+declare const Ng2DropdownButton: any;
+export { Ng2Dropdown, Ng2DropdownMenu, Ng2DropdownButton, Ng2MenuItem };
+declare const NG2_DROPDOWN_DIRECTIVES: any[];
+export { NG2_DROPDOWN_DIRECTIVES };
