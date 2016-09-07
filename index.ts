c5fbf4e0f6f7874e5588a191e90c1f82b6f05534
gbuomprisco <giancarlopsk@gmail.com>
2016-09-07 18:00:42 +0100

    edited index.ts for export

--- index.ts
+++ index.ts
@@ -1,15 +1,7 @@
 const components = require('./dist/ng2-dropdown.bundle.js');
 
-const Ng2Dropdown = components.Ng2Dropdown;
-const Ng2DropdownMenu = components.Ng2DropdownMenu;
-const Ng2MenuItem = components.Ng2MenuItem;
-const Ng2DropdownButton = components.Ng2DropdownButton;
-const Ng2DropdownModule = components.Ng2DropdownModule;
-
-export {
-    Ng2Dropdown,
-    Ng2DropdownMenu,
-    Ng2DropdownButton,
-    Ng2MenuItem,
-    Ng2DropdownModule
-};
+export const Ng2Dropdown = components.Ng2Dropdown;
+export const Ng2DropdownMenu = components.Ng2DropdownMenu;
+export const Ng2MenuItem = components.Ng2MenuItem;
+export const Ng2DropdownButton = components.Ng2DropdownButton;
+export const Ng2DropdownModule = components.Ng2DropdownModule;
