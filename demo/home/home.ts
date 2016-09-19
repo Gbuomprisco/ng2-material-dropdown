c5cf66400fb4a22c8c0fcaaea20e74ce6a23c112
Gbuomprisco <giancarlopsk@gmail.com>
2016-09-19 22:45:52 +0100

    - Fixed demo - Added ability to re-position dropdown if outside page boundaries

--- demo/home/home.ts
+++ demo/home/home.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Ng2Dropdown } from '../../src/components/dropdown/ng2-dropdown';
-import { Ng2DropdownButton } from '../../src/components/button/ng2-dropdown-button';
-import { Ng2DropdownMenu } from '../../src/components/menu/ng2-dropdown-menu';
-import { Ng2MenuItem } from '../../src/components/menu-item/ng2-menu-item';
-import { DropdownContainer } from './components/dropdown-container';
-
 @Component({
     selector: 'app',
     template: require('./home.html')
--- demo/vendor.ts
+++ demo/vendor.ts
@@ -1,6 +1,1 @@
-// Vendors
 
-// Angular 2
-import '@angular/platform-browser-dynamic';
-import '@angular/platform-browser';
-import '@angular/core';
